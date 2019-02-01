35dbbba45bd7040f004a77b4bd14c650749e1acd
casper <cpr1014@gmail.com>
2019-02-01 11:02:14 +0800

    crypto textbook

---
+++ GENERAL/absolute-relative.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python
+# coding=utf-8
+f=open('./strings')
+print(f)
--- REVERSE/picoCTF/keygen-me-2/test.py
+++ REVERSE/picoCTF/keygen-me-2/test.py
@@ -1,1 +1,4 @@
-print(1)
+# coding=utf-8
+encode=[0162, 0157, 0142, 0157, 0164]
+for i in range(len(encode)):
+  print(chr(encode[i]))
