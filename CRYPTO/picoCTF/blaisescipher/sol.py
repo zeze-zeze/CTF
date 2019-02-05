670ae2dd1722e3b4539f9848cc3c06f5b535b359
casper <cpr1014@gmail.com>
2019-02-05 09:40:28 +0800

    blaise cipher little mod

--- CRYPTO/picoCTF/blaisescipher/sol.py
+++ CRYPTO/picoCTF/blaisescipher/sol.py
@@ -3,8 +3,10 @@
 from string import *
 import operator
 
+# 讀檔
 cipher=open('cipher.txt', 'r').read()
 
+# 做一張反查詢的table
 transverse_table={}
 for k in range(2):
   for i in range(26):
@@ -13,11 +15,11 @@ for k in range(2):
       column[chr(0x41+(i-j)%26+0x20*k)]=chr(0x41+j+0x20*k)
     transverse_table[chr(0x41+i+0x20*k)]=column
 
+# 其中有一行有大括號的那個像是picoCTF，就照個那個找到key
 key='flag'
 
-# print transverse_table['f']
+# 把密文轉回
 plaintext=''
-
 j=0
 for i in range(len(cipher)):
   if cipher[i] in ascii_lowercase:
