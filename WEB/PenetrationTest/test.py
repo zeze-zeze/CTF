f636f94358c3bb12981f5012dea61f20012f3419
casper <cpr1014@gmail.com>
2019-02-18 18:21:18 +0800

    picoCTF web secure logon

--- WEB/PenetrationTest/test.py
+++ WEB/PenetrationTest/test.py
@@ -2,16 +2,31 @@
 # coding=utf-8
 import requests
 import json
+import time
 
 # curl -H "Content-Type: application/json" -d '{"key":"zzz"}' -X POST http://localhost:3700/rock2node/createKernelStaff
 access_token='4cibE84UZpLte0lXmKm6At9GzfQEqm_yQDufJX_V9X1tSXL-gUiwbOkgmlEiNUYR0zGW1NfhYdZQu12mlJ_EDHryIZkobENNmFJ34dy1k0a30daHw705e3XFzfdfAIKhveq0JHUDBKV0jCpUfsvPzu1WDtLksv3k6FABChgseM5mKRG75S4NvzLRafLRu4ROq0O5u0kDGC8TNdNkW-_o1Q'
-r=requests.post('https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token='+access_token,
-  # json={"id":10, "name":"Casper", "employee_alias":"zxczxc", "employee_id":"ddd", "mobile":"12345", "email":"ccc", "wechat_id":"asd", "corporation_id":123}
-  json={
-    "userid":"shvk-003",
-    "name":"林哲宇123",
+# r=requests.put('https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token='+access_token
+r=''
+while r=='':
+  try:
+    r=requests.put('http://localhost:4300/rock2node/staffs/3',
+      json={
+        "id":3,
+        "name":"Casper", 
+        "employee_alias":"Engineer/Dev", 
+        "employee_id":"shvk-003", 
+        "mobile":"989325139", 
+        "email":"cpr1014@gmail.com", 
+        "wechat_id":"shvk-003", 
+        "corporation_id":4
+      },
+      verify=False
+    )
+  except:
+    print 'sleep'
+    time.sleep(1)
+# r=requests.delete('http://127.0.0.1:4300/rock2node/staffs')
 
-  }
-)
-# r=requests.get('http://127.0.0.1:3700/rock2node/staffs')
-print r.content.decode('big5')
+# r=requests.get('http://127.0.0.1:4300/rock2node/staffs')
+print r.content.decode('utf-8')
--- WEB/PicoCTF/SecureLogon/SecureLogon.py
+++ WEB/PicoCTF/SecureLogon/SecureLogon.py
@@ -1,58 +1,16 @@
+# /usr/bin/python
 # coding=utf-8
-from Crypto.Cipher import AES
-from Crypto import Random
-from base64 import b64decode
-from base64 import b64encode
-from hashlib import md5
+from base64 import *
 import json
 
-
-secret_key = 'seed removed'
-flag_value = 'flag removed'
-BLOCK_SIZE = 16
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * \
-                chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
-class AESCipher:
-    """
-    Usage:
-        c = AESCipher('password').encrypt('message')
-        m = AESCipher('password').decrypt(c)
-    Tested under Python 3 and PyCrypto 2.6.1.
-    """
-
-    def __init__(self, key):
-        self.key = md5(key.encode('utf8')).hexdigest()
-
-    def encrypt(self, raw):
-        raw = pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return b64encode(iv + cipher.encrypt(raw))
-
-    def decrypt(self, enc):
-        enc = b64decode(enc)
-        iv = enc[:16]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return unpad(cipher.decrypt(enc[16:])).decode('utf8')
-
 cookie={}
-cookie['password']='a'
-cookie['username']='a'
-cookie['admin']=1
-#print(cookie)
-cookie_data=json.dumps(cookie, sort_keys=True)
-encrypted=AESCipher(secret_key).encrypt(cookie_data)
-# print(encrypted)
+cookie['password'] = '123'
+cookie['username'] = '123'
+cookie['admin'] = 0
+cookie_data = json.dumps(cookie, sort_keys=True)
 
-# encrypted='SWAwc7ynqr+u6jzcoHfbN9JUWRO0FzaS1tmxAUrLmOoI0v8mSV8yV7UrLsfCynhy4GCMwy7q4WfrZpLzzAbkZA=='
-blocks=[]
-block=''
-for i in range(0, len(encrypted)):
-    block+=encrypted[i-1]
-    if float(i)/float(15)==0 and not i==0:
-        blocks.append(block)
-        block=''
-print(blocks[0])
-#decrypted=AESCipher(secret_key).decrypt(encrypted)
-#print(decrypted)
+# cipher=b64decode('yOW3euHrSjpgPD/QG51QHNsRFy0hl0Dar5/mykauaDcvWvIldDZlKadrJEYpcLPjTzwPx1hFJlv/N72EG8/gXKVx/OLiROkpXqz3MFtTeDY=')
+# print cipher
+cipher_hex=b64decode('ZI5x2pVCu/rYUSp4iAiP0FUhoajbsunz4N3+2gn+kWe3twlMmkTdvrqwH9QAvexrEhSxtIdhncYb9tqkhl25sjDpv7POu/UC9hBDtqQtNr0=').encode('hex')
+change=hex(int(cipher_hex[20:22], 16)^ord('0')^ord('1'))[2:].zfill(2)
+print b64encode((cipher_hex[:20]+change+cipher_hex[22:]).decode('hex'))
