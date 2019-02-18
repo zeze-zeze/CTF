835df5dc468cd8d0ec1870bd70b16904aed9ba25
casper <cpr1014@gmail.com>
2019-02-18 18:23:08 +0800

    picoCTF web secure logon

--- WEB/PicoCTF/SecureLogon/SecureLogon.py
+++ WEB/PicoCTF/SecureLogon/SecureLogon.py
@@ -9,8 +9,6 @@ cookie['username'] = '123'
 cookie['admin'] = 0
 cookie_data = json.dumps(cookie, sort_keys=True)
 
-# cipher=b64decode('yOW3euHrSjpgPD/QG51QHNsRFy0hl0Dar5/mykauaDcvWvIldDZlKadrJEYpcLPjTzwPx1hFJlv/N72EG8/gXKVx/OLiROkpXqz3MFtTeDY=')
-# print cipher
 cipher_hex=b64decode('ZI5x2pVCu/rYUSp4iAiP0FUhoajbsunz4N3+2gn+kWe3twlMmkTdvrqwH9QAvexrEhSxtIdhncYb9tqkhl25sjDpv7POu/UC9hBDtqQtNr0=').encode('hex')
 change=hex(int(cipher_hex[20:22], 16)^ord('0')^ord('1'))[2:].zfill(2)
 print b64encode((cipher_hex[:20]+change+cipher_hex[22:]).decode('hex'))
