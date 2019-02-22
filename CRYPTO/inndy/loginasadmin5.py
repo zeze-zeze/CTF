d42ae208fba038f2cbd7ab25103ba241ace9a4c3
casper <cpr1014@gmail.com>
2019-02-22 12:46:03 +0800

    inndy crypto login as admin5

---
+++ CRYPTO/inndy/loginasadmin5.py
@@ -0,0 +1,23 @@
+#! python2
+# coding=utf-8
+import requests
+import base64
+import urllib
+
+cipher=base64.b64decode(urllib.unquote('U%2FosUbnY8nSrWz4WPwKSwWPzKq9tOIQ9eCWnN5E%2B').decode('utf-8'))
+plain='{"name":"guest","admin":false}'
+
+s=''
+for a, b in zip(cipher, plain):
+  s+=chr(ord(a) ^ ord(b))
+
+target='{"name":"guest","admin":true}'
+
+new_cookie=''
+for a, b in zip(s, target):
+  new_cookie+=chr(ord(a) ^ ord(b))
+
+new_cookie=urllib.quote(base64.b64encode(new_cookie))
+
+r=requests.get(url='https://hackme.inndy.tw/login5/', cookies={"user5":new_cookie})
+print r.content.decode('utf-8')
