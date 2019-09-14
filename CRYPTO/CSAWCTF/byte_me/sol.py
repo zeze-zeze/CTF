6b92f336e14f8a79501de183bf02f64250fe8929
hardworking_snowman <cpr1014@gmail.com>
2019-09-14 22:41:53 +0800

    CSAWTF pwn TRAVLLER

--- CRYPTO/CSAWCTF/byte_me/sol.py
+++ CRYPTO/CSAWCTF/byte_me/sol.py
@@ -14,23 +14,21 @@ while not pattern.match(flag):
 
         r.sendline('a' * (len(flag) + 1))
         target = r.recvuntil('Tell me something:').split('\n')[1]
+
         r.sendline('a' * (len(flag)))
         test = r.recvuntil('Tell me something:').split('\n')[1]
         
-        #print r.recvall(1)
         print 'target: {}\n  test: {}'.format(target, test)
         if target[:32] != test[:32]:
             print 'found!!'
             for pos in possible:
-                for pos2 in possible:
-                    r.sendline(flag + pos2 + pos)
-                    res = r.recvuntil('Tell me something:').split('\n')[1]
-                    print 'res: {}\n'.format(res)
-                    if res[:32] == answer[:32]:
-                        flag += pos
-                        print '\n\n------------------- {} ------------------\n\n'.format(flag)
-                        open('flag', 'w').write(flag2)
-                        break
+                r.sendline(flag + pos2 + pos)
+                res = r.recvuntil('Tell me something:').split('\n')[1]
+                print 'res: {}\n'.format(res)
+                if res[:32] == answer[:32]:
+                    flag += pos
+                    print '\n\n------------------- {} ------------------\n\n'.format(flag)                         open('flag', 'w').write(flag)
+                    break
         else:
             continue
     except:
