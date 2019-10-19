0005beff0be1386241114284bf18cf1bf5b39a07
hardworking_snowman <cpr1014@gmail.com>
2019-10-19 23:56:32 +0800

    qr

--- CRYPTO/SECCON/ZKPay/sol.py
+++ CRYPTO/SECCON/ZKPay/sol.py
@@ -2,14 +2,20 @@
 import qrtools
 import qrcode
 
-data = open('out.data').read()
+proof = 'MLeHZz3qrQktuT+hWrMdzoyE/Vn8/QkeB4n00T1ZafYKMCAwqwkCRXVfC0Tujs+l5uELgKsULoh4JNO7uMumI01EHCkwCjDq2jhCroy59Hzu8azMjbn6m7ZyqYRwiDPBVrxHeejsBfmm0EqpiTj/RAexkrRFKXph2FAal5HH6/6VsJ7S520eMSAwHpb4MRRQgrpNjT6ORbVgbEL4XJiTekFUqfaIYL6Z+AIwCjCMOR4YqBoyNZndTMs90VjbVnPdftRGhL0sRkn/KUuJCjAgMI+Rc3KanX8wgMGNKGAEIw0IgY42rgschmTwx+2mCignMAowUpaW6WmJiYawnfSRUNr02bOq+vTFTQ7FWFtL1xI0ICMwCjC4L4tM9/ry41v/f51cIYIWLQVQheWDp2qjeaPGMkV/CjEK'
+
+address = 'e1bf7aa7e80687c9e80dfe20d79934c547d1c3fc34a503f06eb48198b121b55a'
+
+data = 'username={}&amount={}&proof={}&hash={}'.format('admin', 10000, proof, address)
+
 qr =qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4,)
 qr.add_data(data)
 qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white") 
-print type(img)
 img.save('admin.png')
 
+'''
 qr = qrtools.QR()
 qr.decode('out.png')
 #print(qr.data)
+'''
