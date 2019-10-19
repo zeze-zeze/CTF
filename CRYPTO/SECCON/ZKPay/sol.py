45d9db9a81c270954906eeb0e5a54cef650ac7ec
hardworking_snowman <cpr1014@gmail.com>
2019-10-19 23:29:01 +0800

    crypto

--- CRYPTO/SECCON/ZKPay/sol.py
+++ CRYPTO/SECCON/ZKPay/sol.py
@@ -1,6 +1,15 @@
 #coding=utf-8
+import qrtools
 import qrcode
-import sys
 
-d = qrcode.Decode()
+data = open('out.data').read()
+qr =qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4,)
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white") 
+print type(img)
+img.save('admin.png')
 
+qr = qrtools.QR()
+qr.decode('out.png')
+#print(qr.data)
